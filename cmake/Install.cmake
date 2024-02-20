bda12d144d318f9d9564a74661be443cd063b5f1
Никита Табакаев <root.kidik@gmail.com>
2024-02-20 22:21:27 +0700

    feat: add trash animation and hero code

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -1,1 +1,3 @@
 install(TARGETS ${PROJECT_NAME})
+execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
+                        ${CMAKE_SOURCE_DIR}/assets ${CMAKE_BINARY_DIR}/assets)
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,118 @@
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Texture.hpp"
+#include "SFML/Window/Event.hpp"
+#include "SFML/Window/Keyboard.hpp"
+
+class GameLoop
+{
+public:
+    GameLoop(sf::RenderWindow& window) : _window{window}
+    {
+        _texture_hero.loadFromFile("../assets/Hero.png");
+        _hero.setTexture(_texture_hero);
+        _animation_time = sf::Time{sf::seconds(0.2)};
+    }
+
+    void run()
+    {
+        while (_window.isOpen())
+        {
+            event_loop();
+            update_loop();
+            render_loop();
+        }
+    }
+
+private:
+    void event_loop()
+    {
+        sf::Event event{};
+        while (_window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                _window.close();
+        }
+    }
+
+    void update_loop()
+    {
+        _dt = _clock.restart();
+        _temp += _dt;
+
+        sf::Vector2i direction{1, 0};
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        {
+            _hero.move(sf::Vector2f{-1, 0} * _speed * _dt.asSeconds());
+            animation(192, 5);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
+            _hero.move(sf::Vector2f{1, 0} * _speed * _dt.asSeconds());
+            animation(128, 5);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        {
+            _hero.move(sf::Vector2f{0, -1} * _speed * _dt.asSeconds());
+            animation(0, 5);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        {
+            _hero.move(sf::Vector2f{0, 1} * _speed * _dt.asSeconds());
+            animation(64, 5);
+        }
+    }
+
+    void render_loop()
+    {
+        _window.clear();
+
+        _window.draw(_hero);
+
+        _window.display();
+    }
+
+    void animation(int y, int frames)
+    {
+        sf::Rect rect{_x, y, 32, 64};
+        _hero.setTextureRect(rect);
+
+        if (_temp.asSeconds() > _animation_time.asSeconds())
+        {
+            _x += 32;
+            if (_x == 32 * frames)
+                _x = 0;
+            _temp = sf::Time::Zero;
+        }
+    }
+
+    enum class Direction
+    {
+        TOP = 1,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
+
+    sf::RenderWindow& _window;
+    sf::Sprite        _hero;
+    sf::Texture       _texture_hero;
+    int               _x = 0;
+    sf::Time          _dt;
+    sf::Time          _temp;
+    sf::Time          _animation_time;
+    sf::Clock         _clock;
+
+    float _speed = 100; // pixels per second
+};
+
 int main()
 {
     sf::Vector2u resolution{640, 400};
@@ -7,50 +120,6 @@ int main()
     sf::RenderWindow window(sf::VideoMode(resolution.x, resolution.y), "VampireSurvivors");
     window.setFramerateLimit(144);
 
-    sf::View view;
-    view.setSize(static_cast<sf::Vector2f>(resolution));
-    view.setCenter(view.getSize() / 2.f);
-
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    float speed = 100.f;
-
-    sf::Clock clock;
-    while (window.isOpen())
-    {
-        sf::Time dt = clock.restart();
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-            shape.move(sf::Vector2f{-1, 0} * speed * dt.asSeconds());
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-            shape.move(sf::Vector2f{1, 0} * speed * dt.asSeconds());
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-            shape.move(sf::Vector2f{0, -1} * speed * dt.asSeconds());
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-            shape.move(sf::Vector2f{0, 1} * speed * dt.asSeconds());
-
-        sf::Event event{};
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-            if (event.type == sf::Event::Resized)
-            {
-                float window_ratio = event.size.width / static_cast<float>(event.size.height);
-                float view_ratio   = view.getSize().x / static_cast<float>(view.getSize().y);
-                view.setSize(view_ratio / window_ratio, window_ratio / view_ratio);
-            }
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+    GameLoop game_loop{window};
+    game_loop.run();
 }
